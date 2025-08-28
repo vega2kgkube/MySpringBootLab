97e40de0745208c5f28e9a6ae210dad228127efd
vega2kgkube <vega2kgkube@gmail.com>
2025-08-29 08:42:37 +0900

    validation 처리

--- src/main/java/com/rookies4/myspringbootlab/exception/advice/DefaultExceptionAdvice.java
+++ src/main/java/com/rookies4/myspringbootlab/exception/advice/DefaultExceptionAdvice.java
@@ -1,17 +1,23 @@
 package com.rookies4.myspringbootlab.exception.advice;
 
 import com.rookies4.myspringbootlab.exception.BusinessException;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -96,5 +102,46 @@ public class DefaultExceptionAdvice {
             return true;
         }
         return false;
+    }
+
+    //입력항목을 검증할때 발생하는 오류를 처리하는 메서드  BAD_REQUEST 400
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationErrorResponse> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult()
+                .getAllErrors()
+                .forEach((error) -> {
+                    //필드명
+                    String fieldName = ((FieldError) error).getField();
+                    //필드에러 메시지
+                    String errorMessage = error.getDefaultMessage();
+                    errors.put(fieldName, errorMessage);
+                });
+
+        ValidationErrorResponse response =
+                new ValidationErrorResponse(
+                        400,
+                        "입력항목 검증 오류",
+                        LocalDateTime.now(),
+                        errors
+                );
+        //badRequest() 400
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    //입력항목을 검증할때 발생한 에러정보를 출력하는 DTO
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class ValidationErrorResponse {
+        //에러코드
+        private int status;
+        //에러메시지
+        private String message;
+        //에러발생한 시간
+        private LocalDateTime timestamp;
+        //개별항목에 에러정보
+        private Map<String, String> errors;
     }
 }
